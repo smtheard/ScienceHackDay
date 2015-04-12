d23285ef13267b38215de13ad9852148e9f2d2dc
Ryan Dickey <ryantay87@gmail.com>
2015-04-12 04:54:39 -0500

    Testing GPS class

--- app/src/main/java/sa/com/drivesafe/GPS.java
+++ app/src/main/java/sa/com/drivesafe/GPS.java
@@ -12,25 +12,57 @@ import android.widget.Toast;
  * Created by USER on 4/12/2015.
  */
 public class GPS {
-    GPS() {
-//
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    GPS(final Activity locManager) {
+        locationManager = (LocationManager) locManager.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener(){
+
+            @Override
+            public void onLocationChanged(Location location) {
+                if(location.getSpeed()>0) {
+                    Toast.makeText(locManager.getApplicationContext(),"yrdy",Toast.LENGTH_LONG);
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
     }
-    private void test(Activity test) {
-        LocationManager locationManager = (LocationManager) test.getSystemService(Context.LOCATION_SERVICE);
+
+}
+
+
+/*
+LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
         LocationListener locationListener = new LocationListener(){
             double prevLat = 0, prevLong = 0, prevTime = 0, timer = 0;
             public void onLocationChanged(Location loc){
-                double param1 = loc.getLatitude() - prevLat;
-                double param2 = loc.getLongitude() - prevLong;
-                timer = System.currentTimeMillis() - prevTime;
-                loc.setSpeed((float)((Math.sqrt((param1*param1) + (param2*param2)) / timer)));
-                prevLat = loc.getLatitude();
-                prevLong = loc.getLongitude();
-                prevTime = System.currentTimeMillis();
-                loc.getLatitude();
-                System.out.println(loc.getSpeed());
-                //Toast.makeText(test.getthis, "Current speed: " + loc.getSpeed(), Toast.LENGTH_SHORT).show();
+                System.out.println("HERE");
+                //double param1 = loc.getLatitude() - prevLat;
+                //double param2 = loc.getLongitude() - prevLong;
+                //timer = System.currentTimeMillis() - prevTime;
+                //loc.setSpeed((float)((Math.sqrt((param1*param1) + (param2*param2)) / timer)));
+                //prevLat = loc.getLatitude();
+                //prevLong = loc.getLongitude();
+                //prevTime = System.currentTimeMillis();
+                //loc.getLatitude();
+                //System.out.println(loc.getSpeed());
+                Toast.makeText(MainActivity.this, "Current speed: " + loc.getSpeed(), Toast.LENGTH_SHORT).show();
             }
             public void onStatusChanged(String provider, int status, Bundle extras) { }
             public void onProviderEnabled(String provider) { }
@@ -38,6 +70,7 @@ public class GPS {
 
         };
 
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-    }
-}
+
+ */
--- app/src/main/java/sa/com/drivesafe/MainActivity.java
+++ app/src/main/java/sa/com/drivesafe/MainActivity.java
@@ -1,14 +1,9 @@
 package sa.com.drivesafe;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.app.Activity;
-import android.content.Context;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -19,29 +14,7 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
-        LocationListener locationListener = new LocationListener(){
-            double prevLat = 0, prevLong = 0, prevTime = 0, timer = 0;
-            public void onLocationChanged(Location loc){
-                double param1 = loc.getLatitude() - prevLat;
-                double param2 = loc.getLongitude() - prevLong;
-                timer = System.currentTimeMillis() - prevTime;
-                loc.setSpeed((float)((Math.sqrt((param1*param1) + (param2*param2)) / timer)));
-                prevLat = loc.getLatitude();
-                prevLong = loc.getLongitude();
-                prevTime = System.currentTimeMillis();
-                loc.getLatitude();
-                System.out.println(loc.getSpeed());
-                Toast.makeText(MainActivity.this, "Current speed: " + loc.getSpeed(), Toast.LENGTH_SHORT).show();
-            }
-            public void onStatusChanged(String provider, int status, Bundle extras) { }
-            public void onProviderEnabled(String provider) { }
-            public void onProviderDisabled(String provider) { }
-
-        };
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        GPS GeoTrack = new GPS(this);
 
     }
 
@@ -66,5 +39,13 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        //locationManager.removeUpdates(this);
+    }
+
 
 }
