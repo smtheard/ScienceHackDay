fa5a46348dc9d46e6424fe5f89fbcc2d2aef63ab
Andrew Gay <gay.carson.andrew@gmail.com>
2015-04-12 01:45:28 -0500

    Basic Message Handling
    TODO: Research more on usage and appropriate implementation. Possibly disable incoming messages while above the milage set.

---
+++ app/src/main/java/sa/com/drivesafe/SmsReceiver.java
@@ -0,0 +1,36 @@
+package sa.com.drivesafe;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+
+public class SmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //---get the SMS message passed in---
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs = null;
+        String messageReceived = "";
+        if (bundle != null) {
+            //---retrieve the SMS message received---
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+            for (int i=0; i<msgs.length; i++) {
+                msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                messageReceived += msgs[i].getMessageBody().toString();
+                messageReceived += "\n";
+            }
+            // Get the Sender Phone Number
+            String senderPhoneNumber=msgs[0].getOriginatingAddress ();
+            sendBack(senderPhoneNumber,"Yay");
+        }
+    }
+
+    public void sendBack(String sender, String msg) {
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(sender, null, msg, null, null);
+    }
+}
