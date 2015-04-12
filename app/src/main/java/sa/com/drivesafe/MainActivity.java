ff2bfccb09d672fbc2ff544a0d106b74305c0fed
Stefan Theard <smtheard@gmail.com>
2015-04-11 23:05:02 -0500

    added permissions

--- app/src/main/java/sa/com/drivesafe/MainActivity.java
+++ app/src/main/java/sa/com/drivesafe/MainActivity.java
@@ -22,16 +22,25 @@ public class MainActivity extends Activity {
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
         LocationListener locationListener = new LocationListener(){
+            double prevLat = 0, prevLong = 0, prevTime = 0, timer = 0;
             public void onLocationChanged(Location loc){
+                double param1 = loc.getLatitude() - prevLat;
+                double param2 = loc.getLongitude() - prevLong;
+                timer = System.currentTimeMillis() - prevTime;
+                loc.setSpeed((float)((Math.sqrt((param1*param1) + (param2*param2)) / timer)));
+                prevLat = loc.getLatitude();
+                prevLong = loc.getLongitude();
+                prevTime = System.currentTimeMillis();
                 loc.getLatitude();
                 System.out.println(loc.getSpeed());
-                Toast.makeText(MainActivity.this, "Current speed:" + loc.getSpeed(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Current speed: " + loc.getSpeed(), Toast.LENGTH_SHORT).show();
             }
             public void onStatusChanged(String provider, int status, Bundle extras) { }
             public void onProviderEnabled(String provider) { }
             public void onProviderDisabled(String provider) { }
 
         };
+
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
     }
